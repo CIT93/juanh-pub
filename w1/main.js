3fb8c34ab398ec90398d94ba0d7011534e25bd54
JuanHuert <dopeog35@gmail.com>
2024-09-02 11:49:22 -0700

    Week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,25 @@
-console.log('hello world');
+console.log("hello from inside main.js file");
+
+//let myVar;
+
+const myVar = 42;
+const myVarType = typeof(myVar);
+console.log("myVarType " + myVarType);
+console.log(`myVarType ${myVarType}`);
+
+if (myVarType ==="string") {
+    console.log(`will 11 run`)
+} else {
+    console.log(`will 13 run`)
+}
+
+function runNow() {
+    if (myVarType ==="string") {
+    console.log(`will 11 run`)
+} else {
+    console.log(`will 13 run`)
+}
+}
+
+runNow();
+runNow();
