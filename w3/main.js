e7cc7e4a514801b01934db5139146d14c4faeb5b
JuanHuert <dopeog35@gmail.com>
2024-09-03 13:57:29 -0700

    working code

--- w3/main.js
+++ w3/main.js
@@ -26,3 +26,27 @@ let carbonFootprintPoints = 0;
 
 totalPoints(3);
 totalPoints(4);
+
+    function houseType(houseSize) {
+    if (houseSize = "large House") {
+        houseNumber = houseNumber + 10;
+    } else if (houseSize = "Medium House") {
+        houseNumber = houseNumber + 7;
+    } else if (houseSize = "small house") {
+        houseNumber = houseNumber + 4;
+    } else if (houseSize = "apartment") {
+        houseNumber = houseNumber + 2;
+    }
+}
+let houseSize = "large";
+let houseNumber = 0;
+console.log();
+console.log(`Based on the house size of ${houseSize} the points would be ${houseNumber}`);
+
+
+
+houseType(10);
+houseType("Large house");
+//let houseSizes = (10);
+//housesSize(7);
+console.log(`The total is ${houseNumber + carbonFootprintPoints}`);
