d4a09a0b35423308045c2c1a9bdd8b553b9c681d
JuanHuert <dopeog35@gmail.com>
2024-09-26 18:40:04 -0700

    my attempt at the code, before watching the solution

--- w7/render.js
+++ w7/render.js
@@ -1,6 +1,10 @@
 const TBL = document.getElementById("tab-data");
 
 function renderTblHeading() {
+  let existingTable = document.querySelector("table");
+  if (existingTable) {
+    return existingTable;
+  }
   const table = document.createElement("table");
   const thead = document.createElement("thead");
   const tr = document.createElement("tr");
@@ -15,16 +19,21 @@ function renderTblHeading() {
   return table;
 }
 
-function renderTbl(data) {
-  const table = renderTblHeading(); 
+function renderTbl(dataArray) {
+  const table = renderTblHeading();
   const tbody = document.createElement("tbody");
   const tr = document.createElement("tr");
-  const trTextArr = ["juan", 6, "large", 14];
-  trTextArr.forEach(function (text) {
+  //const trTextArr = ["juan", 6, "large", 14];
+  dataArray.forEach(function (text) {
     const td = document.createElement("td");
     td.textContent = text;
     tr.appendChild(td);
   });
+  tbody.appendChild(tr);
+  table.appendChild(tbody);
+  
+
+  
   const td = document.createElement("td");
   const btnEdit = document.createElement("button");
   const btnDel = document.createElement("button");
@@ -37,5 +46,16 @@ function renderTbl(data) {
   table.appendChild(tbody);
   TBL.appendChild(table);
 }
-
+return table;
 export { renderTbl, renderTblHeading };
+
+// function renderTbl(data) {
+//   const table = renderTblHeading(); 
+//   const tbody = document.createElement("tbody");
+//   const tr = document.createElement("tr");
+//   const trTextArr = ["juan", 6, "large", 14];
+//   trTextArr.forEach(function (text) {
+//     const td = document.createElement("td");
+//     td.textContent = text;
+//     tr.appendChild(td);
+//   });
